60442e51c034b598233428cbd21d8b92805b200d
Danish Ahmed <ahmed.danish800@gmail.com>
2025-02-26 21:03:50 +0500

    fix(deep copy and shallow copy): Previously I ahd just demonstrated deep copy now I fixed it to demonstrate both deep copy and shallow copy

--- practice.js
+++ practice.js
@@ -1,13 +1,35 @@
-let arr = [1,2,3,4,5,6,7,8,9,10];
+  //  SHALLOW COPY
 
-let newArr = JSON.parse(JSON.stringify(arr));
+  console.log("Shallow Copy");
 
-console.log("Arr=>", arr);
-console.log("NewArr=>", newArr);
+let shallowArr = [1,2,3,4,5,6,7,8,9,10];
+
+let newShallowArr = shallowArr;
+
+console.log("Arr=>", shallowArr);
+console.log("NewArr=>", newShallowArr);
 
 console.log("Modifying newArr");
 
-newArr[0] = 100;
+newShallowArr[0] = 100;
 
-console.log("Arr=>", arr);
-console.log("NewArr=>", newArr);
+console.log("Arr=>", shallowArr);
+console.log("NewArr=>", newShallowArr);
+
+// DEEP COPY
+
+console.log("Deep Copy");
+
+let deepArr = [1,2,3,4,5,6,7,8,9,10];
+
+let newDeepArr = JSON.parse(JSON.stringify(deepArr));
+
+console.log("Arr=>", deepArr);
+console.log("NewArr=>", newDeepArr);
+
+console.log("Modifying newDeepArr");
+
+newDeepArr[0] = 100;
+
+console.log("Arr=>", deepArr);
+console.log("NewArr=>", newDeepArr);
